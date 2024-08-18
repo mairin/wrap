81e6226fbbb1cf52732dfcd00d9c8b2b334d682f
Máirín Duffy <duffy@redhat.com>
2024-08-18 11:50:42 -0400

    Add multi-file support + strip extra newlines
    Added multi-file support so you can run `python3 wrap.py $DIR/*` and it
    will handle it, using the original input filename and outputting a
    wrapped version of the file with the suffix '.wrap.md'
    
    Also strips extraneous newlines from input files.
    
    Signed-off-by: Máirín Duffy <duffy@redhat.com>

--- wrap.py
+++ wrap.py
@@ -1,5 +1,6 @@
 import textwrap
 import sys
+import os
 
 def wrap_text(input_file, width=72):
     with open(input_file, 'r') as file:
@@ -9,20 +10,21 @@ def wrap_text(input_file, width=72):
     paragraphs = content.split('\n')
 
     for paragraph in paragraphs:
-        if paragraph.strip() == "":
-            wrapped_lines.append("")
-        else:
+        if paragraph.strip():
             wrapped_paragraph = textwrap.fill(paragraph, width=width)
             wrapped_lines.append(wrapped_paragraph)
 
     wrapped_content = '\n\n'.join(wrapped_lines)
-
-    sys.stdout.write(wrapped_content)
+   
+    output_file_tuple = os.path.splitext(input_file)
+    output_file = f"{output_file_tuple[0]}.wrap{output_file_tuple[1]}"
+    with open(output_file, 'w') as file:
+        file.write(wrapped_content)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("python3 wrap.py <input_file>")
         sys.exit(1)
 
-    input_file = sys.argv[1]
-    wrap_text(input_file)
+    for input_file in sys.argv[1:]:
+        wrap_text(input_file)
